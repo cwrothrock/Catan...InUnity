9325a57cdc286159a16d510bdd3dd3e76a2b2bba
Charles W. Rothrock <chaz249@hotmail.com>
2023-08-09 18:55:33 -0400

    Baseline tile iteartion for resource identification

--- Assets/Scripts/Managers/CatanManager.cs
+++ Assets/Scripts/Managers/CatanManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class CatanManager : MonoBehaviour
 {
+    [SerializeField] private Tilemap tilemapResources;
+    [SerializeField] private GameObject placePointPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tilemapResources.CompressBounds();
+        FindResourcePositions();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FindResourcePositions()
+    {
+        for (int x = tilemapResources.cellBounds.xMin; x < tilemapResources.cellBounds.xMax; x++) {
+            for (int y = tilemapResources.cellBounds.yMin; y < tilemapResources.cellBounds.yMax; y++) {
+                Vector3Int pos = new Vector3Int(x,y,(int)tilemapResources.transform.position.z);
+                if (tilemapResources.HasTile(pos)) {
+                    Debug.Log(new Vector3(x,y,0));
+                    Debug.Log(tilemapResources.GetSprite(pos));
+                    Object.Instantiate(placePointPrefab, tilemapResources.GetCellCenterWorld(pos), Quaternion.identity);
+                }
+            }
+        }
     }
 }
