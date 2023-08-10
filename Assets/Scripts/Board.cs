a9553021b45523c83653e04d24140c2088d0a177
Charles W. Rothrock <chaz249@hotmail.com>
2023-08-10 19:07:06 -0400

    Add CharlesDev scene

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateBoard : MonoBehaviour
+public class Board : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
