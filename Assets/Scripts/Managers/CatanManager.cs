3134b122adccb8fc465150f2af6195cdc214ec16
Daniel Arn <arn@andrews.edu>
2023-08-11 18:25:37 -0400

    Update CatanManager.cs
    Update manager class

--- Assets/Scripts/Managers/CatanManager.cs
+++ Assets/Scripts/Managers/CatanManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.Tilemaps;
 public class CatanManager : MonoBehaviour
 {
     [SerializeField]
+    private Tilemap tilemapResources;
+    [SerializeField]
+    private Tilemap tilemapNumbers;
+
+    [SerializeField]
     private Tile desertTile;
     [SerializeField]
     private Tile brickTile;
@@ -64,6 +69,12 @@ public class CatanManager : MonoBehaviour
         public ResourceType resourceType;
         public int diceNumber;
 
+        public BoardTile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
         public BoardTile(int x, int y, ResourceType resourceType, int diceNumber)
         {
             this.x = x;
@@ -79,38 +90,58 @@ public class CatanManager : MonoBehaviour
         private Tilemap numberGrid;
         private List<BoardTile> boardTiles;
 
-        public Board()
+        public Board(Tilemap resources, Tilemap numbers)
         {
-            boardTiles = new();
-            resourceGrid = GameObject.FindGameObjectWithTag("ResourceTiles").GetComponent<Tilemap>();
-            numberGrid = GameObject.FindGameObjectWithTag("NumberTiles").GetComponent<Tilemap>();
-        }
+            resourceGrid = resources;
+            numberGrid = numbers;
 
-        public void GenerateBoard(int x_from, int x_to, int y_from, int y_to)
-        {
-            for (int x = x_from; x <= x_to; x++)
+            boardTiles = new();
+
+            for (int x = resourceGrid.cellBounds.xMin; x < resourceGrid.cellBounds.xMax; x++)
             {
-                for (int y = y_from; y <= y_to; y++)
+                for (int y = resourceGrid.cellBounds.yMin; y < resourceGrid.cellBounds.yMax; y++)
                 {
-                    BoardTile boardTile = new(x, y, ResourceType.WOOD, 6);
+                    // TODO: I think this only works for small boards. Figure out how to do this for abstract boards.
+                    if (x == resourceGrid.cellBounds.xMin && Mathf.Abs(x) == Mathf.Abs(y))
+                    {
+                        continue;
+                    }
+                    if (x == resourceGrid.cellBounds.xMax - 1 && y != 0)
+                    {
+                        continue;
+                    }
+
+                    BoardTile boardTile = new(x, y);
                     boardTiles.Add(boardTile);
                 }
             }
         }
 
-        public void PopulateBoard()
+        public void GenerateBoard()
         {
             foreach (BoardTile boardTile in boardTiles)
             {
-                resourceGrid.SetTile(new Vector3Int(boardTile.x, boardTile.y, 0), resourceTiles[(int)boardTile.resourceType]);
-                numberGrid.SetTile(new Vector3Int(boardTile.x, boardTile.y, 0), numberTiles[boardTile.diceNumber]);
+                // TODO: Add board generation logic
+                boardTile.resourceType = (ResourceType)Random.Range(0, 5);
+                boardTile.diceNumber = Random.Range(2, 12);
+
+                SetBoardTile(boardTile);
             }
+        }
+
+        public void SetBoardTile(BoardTile boardTile)
+        {
+            resourceGrid.SetTile(new Vector3Int(boardTile.x, boardTile.y, 0), resourceTiles[(int)boardTile.resourceType]);
+            numberGrid.SetTile(new Vector3Int(boardTile.x, boardTile.y, 0), numberTiles[boardTile.diceNumber]);
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        tilemapResources.CompressBounds();
+        tilemapNumbers.CompressBounds();
+
         resourceTiles = new Tile[] {
             desertTile,
             brickTile,
@@ -134,9 +165,8 @@ public class CatanManager : MonoBehaviour
             dice12Tile
         };
 
-        board = new();
-        board.GenerateBoard(-2, 2, -2, 2);
-        board.PopulateBoard();
+        board = new(tilemapResources, tilemapNumbers);
+        board.GenerateBoard();
     }
 
     // Update is called once per frame
