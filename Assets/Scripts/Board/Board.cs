91e6058390deb2c6b845e6c37a901cdae1fb2b23
Daniel Arn <arn@andrews.edu>
2023-08-16 22:34:47 -0400

    Update Board.cs

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -79,7 +79,7 @@ public class Board : MonoBehaviour
 
     private List<BoardTile> boardTiles = new List<BoardTile>();
 
-    // Graph Classes
+    // Graph
     private class Node { }
 
     private class VertexNode : Node
@@ -103,7 +103,10 @@ public class Board : MonoBehaviour
         public TileNode() { }
     }
 
-    // end Graph Classes
+    private VertexNode[] vertexNodes;
+    private EdgeNode[] edgeNodes;
+    private TileNode[] tileNodes;
+    // end Graph
 
     private void Start()
     {
