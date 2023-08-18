3adccad8521a8f6b7dcdf7e6eb2e4fc90f58f67e
Daniel Arn <arn@andrews.edu>
2023-08-17 21:48:06 -0400

    Update Graph.cs
    Add `None` building type.
    Make member variables private for classes.
    Add more getters and setters.

--- Assets/Scripts/Board/Graph.cs
+++ Assets/Scripts/Board/Graph.cs
@@ -1,80 +1,96 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 public class Graph
 {
     public enum Building
     {
+        None,
         Settlement,
         City,
     }
 
-    public class Node { }
+    public class Node
+    {
+        private Vector3 worldPosition;
+
+        public Vector3 GetWorldPosition() { return worldPosition; }
+
+        public void SetWorldPosition(Vector3 worldPosition) { this.worldPosition = worldPosition; }
+    }
 
     public class VertexNode : Node
     {
-        public List<EdgeNode> edges = new();
-        public Building building;
-        public string owner;
+        private List<EdgeNode> edges = new();
+        private Building building = Building.None;
+        private string owner;
 
         public VertexNode() { }
 
-        public void AddEdge(EdgeNode edge)
-        {
-            edges.Add(edge);
-        }
+        public void AddEdge(EdgeNode edge) { edges.Add(edge); }
+
+        public List<EdgeNode> GetEdges() { return edges; }
+
+        public Building GetBuilding() { return building; }
+
+        public void SetBuilding(Building building) { this.building = building; }
+
+        public string GetOwner() { return owner; }
+
+        public void SetOwner(string owner) { this.owner = owner; }
     }
 
     public class EdgeNode : Node
     {
-        public List<VertexNode> vertices = new();
-        public bool hasRoad = false;
-        public string owner;
+        private List<VertexNode> vertices = new();
+        private string owner;
 
         public EdgeNode() { }
 
-        public void AddVertex(VertexNode vertex)
-        {
-            vertices.Add(vertex);
-        }
+        public void AddVertex(VertexNode vertex) { vertices.Add(vertex); }
+
+        public List<VertexNode> GetVertices() { return vertices; }
+
+        public string GetOwner() { return owner; }
+
+        public void SetOwner(String owner) { this.owner = owner; }
     }
 
     public class TileNode : Node
     {
-        public List<VertexNode> vertices = new();
-        public bool hasRobber = false;
+        private List<VertexNode> vertices = new();
+        private bool hasRobber = false;
 
         public TileNode() { }
 
-        public void AddVertex(VertexNode vertex)
-        {
-            vertices.Add(vertex);
-        }
+        public void AddVertex(VertexNode vertex) { vertices.Add(vertex); }
+
+        public List<VertexNode> GetVertices() { return vertices; }
+
+        public bool HasRobber() { return hasRobber; }
+
+        public void SetHasRobber(bool hasRobber) { this.hasRobber = hasRobber; }
     }
 
-    public List<VertexNode> vertices = new();
-    public List<EdgeNode> edges = new();
-    public Dictionary<Vector3Int, TileNode> tiles = new();
+    private List<VertexNode> vertices = new();
+    private List<EdgeNode> edges = new();
+    private Dictionary<Vector3Int, TileNode> tiles = new();
 
     public Graph() { }
 
-    public void AddVertex(VertexNode vertex)
-    {
-        vertices.Add(vertex);
-    }
+    public void AddVertex(VertexNode vertex) { vertices.Add(vertex); }
 
-    public void AddEdge(EdgeNode edge)
-    {
-        edges.Add(edge);
-    }
+    public void AddEdge(EdgeNode edge) { edges.Add(edge); }
 
-    public void AddTile(Vector3Int position, TileNode tile)
-    {
-        tiles[position] = tile;
-    }
+    public void AddTile(Vector3Int position, TileNode tile) { tiles[position] = tile; }
 
-    public TileNode GetTileAt(Vector3Int position)
-    {
-        return tiles[position];
-    }
+    public TileNode GetTileAt(Vector3Int position) { return tiles[position]; }
+
+    public List<VertexNode> GetVertices() { return vertices; }
+
+    public List<EdgeNode> GetEdges() { return edges; }
+
+    public List<TileNode> GetTiles() { return tiles.Values.ToList(); }
 }
