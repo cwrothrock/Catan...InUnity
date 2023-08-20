cc7dfbb4b72a3c283abf2364d40a138333102d54
Charles W. Rothrock <chaz249@hotmail.com>
2023-08-20 08:29:55 -0400

    Add logic to avoid 6-8 adjacencies

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using AYellowpaper.SerializedCollections;
 using System.Collections.Generic;
+using UnityEditor;
+using System.Linq;
 
 public class Board : MonoBehaviour
 {
@@ -77,55 +79,111 @@ public class Board : MonoBehaviour
     [SerializeField] private TextAsset boardVariantJsonAsset;
     private BoardVariant boardVariant;
 
-    private List<BoardTile> boardTiles = new List<BoardTile>();
+    private List<TerrainTile> terrainTiles = new List<TerrainTile>();
+    private List<PortTile> portTiles = new List<PortTile>();
 
     private Graph graph;
 
     private void Start()
     {
-        // docksTilemap.CompressBounds();
-        // landTilemap.CompressBounds();
-
         boardVariant = BoardVariant.From(boardVariantJsonAsset);
         // boardVariant.portPositions = new();
         // boardVariant.landPositions = new();
 
         GenerateBoard();
+        UpdateBoardUI();
     }
 
     private void GenerateBoard()
     {
-        List<TileType> terrainOrder = BoardVariant.Explode(boardVariant.terrainCounts, shuffle: true);
-        List<PortType> portOrder = BoardVariant.Explode(boardVariant.portCounts, shuffle: true);
-        List<int> diceOrder = BoardVariant.Explode(boardVariant.diceCounts, shuffle: true);
+        List<TileType> terrainOrder = BoardVariant.Explode(boardVariant.terrainCounts, shuffle: false);
+        List<int> diceOrder = BoardVariant.Explode(boardVariant.diceCounts, shuffle: false);
 
-        foreach (Vector3Int pos in boardVariant.landPositions)
+        // Place 6/8s first
+        int sixes = diceOrder.RemoveAll(x => x == 6);
+        int eights = diceOrder.RemoveAll(x => x == 8);
+        int count = sixes + eights;
+        List<Vector3Int> candidatePositions;
+        do {
+            BoardVariant.Shuffle(boardVariant.landPositions);
+            candidatePositions = boardVariant.landPositions.Take(count).ToList();
+        } while (HasNeighbors(candidatePositions));
+        
+        List<int> diceTemp = Enumerable.Repeat(6, sixes).ToList();
+        diceTemp.AddRange(Enumerable.Repeat(8, eights));
+        for (int i = 0; i < count; i++)
         {
-            landTilemap.SetTile(pos, terrainTilesDict[TileType.LAND]);
-
-            TerrainTile terrainTile = new TerrainTile(pos, terrainOrder[0]);
-            terrainOrder.RemoveAt(0);
-            terrainTilemap.SetTile(pos, terrainTilesDict[terrainTile.tileType]);
-
-            if (!terrainTile.tileType.Equals(TileType.DESERT))
+            if (terrainOrder[i].Equals(TileType.DESERT))
             {
-                terrainTile.diceNumber = diceOrder[0];
+                AddTerrainTile(candidatePositions[i], terrainOrder[i], 7);
+            } else 
+            {
+                AddTerrainTile(candidatePositions[i], terrainOrder[i], diceTemp[i]);
                 diceOrder.RemoveAt(0);
-                numbersTilemap.SetTile(pos, numberTilesDict[terrainTile.diceNumber]);
             }
-
-            boardTiles.Add(terrainTile);
-            // boardVariant.landPositions.Add(pos);
         }
 
+        // Place remaining numbers
+        for (int i = count; i < terrainOrder.Count; i++)
+        {
+            if (terrainOrder[i].Equals(TileType.DESERT))
+            {
+                AddTerrainTile(boardVariant.landPositions[i], terrainOrder[i], 7);
+            } else 
+            {
+                AddTerrainTile(boardVariant.landPositions[i], terrainOrder[i], diceOrder[0]);
+                diceOrder.RemoveAt(0);
+            }
+        }
+
+        // Place ports
+        List<PortType> portOrder = BoardVariant.Explode(boardVariant.portCounts, shuffle: true);
         foreach (Vector3Int pos in boardVariant.portPositions)
         {
-            PortTile portTile = new PortTile(pos, portOrder[0]);
+            AddPortTile(pos, portOrder[0]);
             portOrder.RemoveAt(0);
-            portsTilemap.SetTile(pos, portTilesDict[portTile.portType]);
+        }
+    }
 
-            boardTiles.Add(portTile);
-            // boardVariant.portPositions.Add(pos);
+    private bool HasNeighbors(List<Vector3Int> positions)
+    {
+        for (int i = 0; i < positions.Count; i++)
+        {
+            for (int j = i+1; j < positions.Count; j++)
+            {
+                // if (IsNeighbor(positions[i], positions[j]))
+                // {
+                //     return true;
+                // }
+            }
+        }
+        return false;
+    }
+
+    private void AddTerrainTile(Vector3Int position, TileType type, int diceNumber)
+    {
+        terrainTiles.Add(new TerrainTile(position, type, diceNumber));
+    }
+
+    public void AddPortTile(Vector3Int position, PortType type)
+    {
+        portTiles.Add(new PortTile(position, type));
+    }
+
+    private void UpdateBoardUI()
+    {
+        foreach (TerrainTile tile in terrainTiles)
+        {
+            landTilemap.SetTile(tile.position, terrainTilesDict[TileType.LAND]);
+            terrainTilemap.SetTile(tile.position, terrainTilesDict[tile.tileType]);
+            if (!tile.tileType.Equals(TileType.DESERT))
+            {
+                numbersTilemap.SetTile(tile.position, numberTilesDict[tile.diceNumber]);
+            }
+        }
+        foreach (PortTile tile in portTiles)
+        {
+            portsTilemap.SetTile(tile.position, portTilesDict[tile.portType]);
         }
     }
 }
